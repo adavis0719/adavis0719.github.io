18b767349b912c21e95752f84a62b9440e4c7098
adavis0719 <71847730+adavis0719@users.noreply.github.com>
2020-10-30 18:48:28 -0500

    IMG_Gallery

---
+++ image_gallery/image_gallery/image_gallery.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    // preload images
+    $("#image_list a").each(function() {
+        var swappedImage = new Image();
+        swappedImage.src = $(this).attr("href");
+        swappedImage.title = $(this).attr("title");
+    });
+    
+    
+    //set up event handlers for links
+    $("#image_list a").click(function(evt){
+        //swap image
+        var imageURL = $(this).attr("href");
+        $("#image").attr("src", imageURL);
+        
+        //swap caption
+        var caption = $(this).attr("title");
+        $("#caption").text(caption);
+
+        evt.preventDefault();
+        
+    });
+    
+    $("li:first-child a").focus();
+});
